3c5e7309529f40d30a350f7b4c56b945627daf40
egbuniwecemeka <egbuniwecemeka.com>
2024-11-08 02:00:05 +0100

    Consistent hard work pays

---
+++ 0x00-personal_data/study/0-log.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" script to test the basic logging """
+
+import logging
+
+# Setup logger config
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='0-log.log', encoding='utf-8', level=logging.DEBUG)
+
+logger.debug('Log mess to log file')
+logger.info('Also this')
+logger.warning('and warnings to')
+logger.error('Add non-AsCII chacters also')
