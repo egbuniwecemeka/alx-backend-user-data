8ce95b1b918dc8892be242269e59d3cd22ba487d
egbuniwecemeka <egbuniwecemeka.com>
2024-11-10 20:17:53 +0100

    Regex-ing main file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Returns an obfuscated log message """
     regex = fr"({'|'.join(fields)})={separator}*[^ {separator}]*"
     return re.sub(regex, lambda m: f"{m.group(1)}={redaction}", message)
