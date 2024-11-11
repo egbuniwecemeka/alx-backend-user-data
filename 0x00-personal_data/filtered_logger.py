854311908759637d0426fb2ea774ee05fe8734c9
egbuniwecemeka <egbuniwecemeka.com>
2024-11-11 06:15:02 +0100

    Regex-ing study

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,22 @@
 
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)s %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List):
+        super(RedactingFormatter).__init__(self.FORMAT)
+        self.fields = fields
+    
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
