82fb12f5b5f10221a3934b58212511dea5dbeb01
egbuniwecemeka <egbuniwecemeka.com>
2024-11-12 21:59:25 +0100

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -58,7 +58,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.MySQLConnector:
     """Returns a connector to a database"""
     connect = mysql.connector.connect(
         host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
