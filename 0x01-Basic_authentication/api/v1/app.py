5767c06be5e04d37d635c7d774be73d5ae778fed
egbuniwecemeka <egbuniwecemeka.com>
2024-11-17 13:58:24 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,19 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# Initialize auth based on environment variables
+auth = None
+AUTH_TYPE = getenv('AUTH_TYPE')
+if AUTH_TYPE == auth:
+    auth = Auth()
+
 
 
 @app.errorhandler(404)
