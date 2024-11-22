877a256f8d1decd2237dacd5d8ae41ebb15f8ef5
egbuniwecemeka <egbuniwecemeka.com>
2024-11-22 18:24:55 +0100

    Basic = Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -58,3 +59,8 @@ class BasicAuth(Auth):
             ':', 1
         )
         return user_email, user_password
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Returns the user instance based on his email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
