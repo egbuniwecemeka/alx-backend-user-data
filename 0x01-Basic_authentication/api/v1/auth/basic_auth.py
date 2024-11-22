8d820339beeead86728e98492e1cf0344638f5f6
egbuniwecemeka <egbuniwecemeka.com>
2024-11-22 02:00:08 +0100

    Basic = Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,8 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Authentication class"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Returns the Base64 part of the Authorization header"""
+        if authorization_header is None:
+            return None
