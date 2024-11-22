a72f6795798af5f48ffda0db603bed26106cb7ad
egbuniwecemeka <egbuniwecemeka.com>
2024-11-22 15:39:49 +0100

    Basic = Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,3 +54,6 @@ class BasicAuth(Auth):
         
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
+        
+        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_password
