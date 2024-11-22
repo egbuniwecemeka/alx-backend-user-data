c2cab0cfd10777d46f4c323e349395737cb23dc7
egbuniwecemeka <egbuniwecemeka.com>
2024-11-22 02:58:06 +0100

    Basic = Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """BasicAuth Class"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,23 @@ class BasicAuth(Auth):
 
         # Extract and return the basic part after `Basic `
         return authorization_header[6:]
+
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        
+        if not isinstance(base64_authorization_header, str):
+            return None
+        
+        try:
+            # Decode Base64 header
+            decoded_bytes = base64.base64decode(base64_authorization_header)
+
+            # Return decoded byte as UTF8 string
+            return decoded_bytes.decode('utf8')
+        except(base64.binascii.Error, UnicodeDecodeError):
+            # Return None if decoding fails
+            return None
